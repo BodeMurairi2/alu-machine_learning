d6611ed71711f41c161da550819493138e9643cc
BodeMurairi <bodemurairi2@gmail.com>
2026-05-08 12:38:23 +0200

    startinng stcking bars

---
+++ math/plotting/6-bars.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+This module computes a stacking bar
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+fruit = np.random.randint(0, 20, (4,3))
+
+# your code here
