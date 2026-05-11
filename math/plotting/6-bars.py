c418bee9b53fc924de532e6917736a771212e5f4
BodeMurairi <bodemurairi2@gmail.com>
2026-05-11 11:16:48 +0200

    question 6 complete

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -10,4 +10,20 @@ import matplotlib.pyplot as plt
 np.random.seed(5)
 fruit = np.random.randint(0, 20, (4,3))
 
-# your code here
+people = ['Farrah', 'Fred', 'Felicia']
+colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
+labels = ['apples', 'bananas', 'oranges', 'peaches']
+x = np.arange(len(people))
+
+bottoms = np.zeros(len(people))
+for i, (color, label) in enumerate(zip(colors, labels)):
+    plt.bar(x, fruit[i], width=0.5, bottom=bottoms, color=color, label=label)
+    bottoms += fruit[i]
+
+plt.xticks(x, people)
+plt.yticks(np.arange(0, 81, 10))
+plt.ylim(0, 80)
+plt.ylabel('Quantity of Fruit')
+plt.title('Number of Fruit per Person')
+plt.legend()
+plt.show()
