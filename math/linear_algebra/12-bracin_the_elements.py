e8782cc57de7c1a56ddd0435cab6310d1ef1cb0d
BodeMurairi2 <b.murairi@alustudent.com>
2026-05-18 00:31:05 +0200

    add, sub, mult, and division of matrices

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -8,16 +8,13 @@ def np_elementwise(mat1, mat2):
     """
     This function adds, subs, mult, and divide
     """
-    shape_1 = mat1.shape()
-    shape_2 = mat2.shape()
-    if shape_1 != shape_2:
-        raise ValueError("Not the same shape")
-    add_vector = mat1 + mat2
-    sub_vector = mat1 - mat2
-    if shape_1[1] != shape_2[0]:
-        raise ValueError("number of columns in a is not equal to number of rows of b")
-    mult_vector = mat1 * mat2
-    div_vector = mat1 / mat2
+    try:
+        add_vector = mat1 + mat2
+        sub_vector = mat1 - mat2
+        mult_vector = mat1 * mat2
+        div_vector = mat1 / mat2
+    except Exception as error:
+        raise error
     return (
         add_vector,
         sub_vector,
