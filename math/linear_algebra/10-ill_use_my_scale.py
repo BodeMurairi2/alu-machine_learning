d53935635d229a13485482a394b0ec76e5efef72
BodeMurairi2 <b.murairi@alustudent.com>
2026-05-18 00:12:43 +0200

    shape of ndarray

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python3
-"""
-This module returns the shape of nd.array
-"""
+"""This module returns the shape of a numpy array."""
 
 
-import numpy as np
-
-
-def np_shape(matrix: np.ndarray) -> tuple:
-    """
-    This function returns the shape of an array
-    matrix:np.array
-    return tuple
-    """
-    return np.shape(matrix)
+def np_shape(matrix):
+    """Returns the shape of a numpy array as a tuple of integers."""
+    return matrix.shape
