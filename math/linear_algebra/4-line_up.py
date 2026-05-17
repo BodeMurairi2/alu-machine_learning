18595624e6060498c94cec06b3f0f90531066868
BodeMurairi2 <b.murairi@alustudent.com>
2026-05-17 23:31:44 +0200

    adding two vectors

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+"""
+This module adds two list(element wise)
+"""
+
+from typing import List, Union
+
+
+def add_arrays(arr1: List[Union[int, float]], arr2: List[Union[int, float]]):
+    """
+    This function adds two arrays
+    Args:
+        arr1:List[float|int]
+        arr2:List[float|int]
+    return
+        arr3:List[float|int]
+    """
+    row_a = len(arr1)
+    row_b = len(arr2)
+    arr3 = []
+    if row_a != row_b:
+        return None
+
+    if not isinstance(arr1[0], list) and not isinstance(arr2[0], list):
+        for i, j in zip(arr1, arr2):
+            arr3.append(i + j)
+        return arr3
+
+    dim_a = [row_a, len(arr1[0])]
+    dim_b = [row_b, len(arr2[0])]
+
+    if dim_a != dim_b:
+        return None
+
+    for i, j in zip(arr1, arr2):
+        arr3.append([a + b for a, b in zip(i, j)])
+    return arr3
