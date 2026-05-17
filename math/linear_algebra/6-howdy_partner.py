83f65f21cff309fdffc8f6ad32f9676949a608c7
BodeMurairi2 <b.murairi@alustudent.com>
2026-05-17 23:47:36 +0200

    combine two arrays

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Function that concatenate two arrays
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    This function combines two arrays
+    """
+    return arr1 + arr2
